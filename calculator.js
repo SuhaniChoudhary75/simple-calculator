badb89049c5023031c25f0d00b76ef4fc1ddcdc8
SuhaniChoudhary75 <158177472+SuhaniChoudhary75@users.noreply.github.com>
2024-08-30 09:21:46 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,15 @@
+function calculate(operation) {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    let result;
+
+    if (operation === 'add') {
+        result = num1 + num2;
+    } else if (operation === 'subtract') {
+        result = num1 - num2;
+    } else if (operation === 'multiply') {
+        result = num1 * num2;
+    }
+
+    document.getElementById('result').innerText = 'Result: ' + result;
+}
